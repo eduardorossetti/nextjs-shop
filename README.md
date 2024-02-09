2af7dda306ce1544d25d234214f69f35563faf1b
eduardorossetti <edurossettimelo@hotmail.com>
2024-02-09 11:10:23 -0300

    add: Setup stitches

---
+++ README.md
@@ -0,0 +1,7 @@
+# Histórico de Alterações
+
+## 09/02/2024
+
+### Adições
+
+- Configurando Stitches para estilização de componentes
---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, css, styled, globalCss, keyframes, getCssText, theme, createTheme } = createStitches({
+  theme: {
+    colors: {
+      default: '#8257e6',
+    },
+  },
+})
